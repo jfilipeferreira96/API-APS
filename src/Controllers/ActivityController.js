5a0cc92236cbf0844ff35d8de14539119da7a172
José Filipe Ferreira <jose.ferreira@i-charging.pt>
2022-11-17 16:56:36 +0000

    new changes were made

--- src/Controllers/ActivityController.js
+++ src/Controllers/ActivityController.js
@@ -1,44 +1,82 @@
 const path = require("path");
 
-class ActivityController {
+class ActivityController{
+  //1.parâmetros respetivos
   async SendParams(req, res) {
     //const { ativityID } = req.body;
 
     //Implementar busca na BD que devolve os parametros da atividade com id xxxx
 
-    const sendTestParams = {
-      s1: "Setting 1",
-      s2: "Setting 2",
-      s3: "Setting 3",
-    };
+    const sendTestParams = [
+      {"name": "title", "type": "text/plain"},
+    ]
 
     return res.status(200).json(sendTestParams);
   }
 
-  async SendActivityPage(req, res) {
-    //const { studentID } = req.body;
+  //3. Deploy de atividade
+  //− user_url: URL para um Web service que permite efetuar o deploy da atividade, recebendo como parâmetro uma identificação da instância na Inven!RA;
+  async SendActivityPageURL(req, res) {
+    const { activityID, inveniraStdID, json_params } = req.body;
 
-    //Implementar alguma maneira de guardar e enviar este valor para a página do aluno
+    //devo armazenar o id da atividade na bd
+    //não deve enviar a pagina mas sim um url com a pagina!
     let indexPath = path.join(__dirname, "../Views/activity_page.html");
     return res.sendFile(indexPath);
   }
 
+  async SendActivityPageURL(req, res) {
+  const { activityID, inveniraStdID, json_params } = req.body;
+
+    //pesquisa na bd e envia informação para a pagina do aluno
+    const data = {
+      
+    }
+    res.json(data);
+  }
+  
+  //2. Lista de analytics da atividade
+  // analytics_list_url: URL para um Web service que devolve a lista de analytics que o Activity Provider recolherá
   async GetAnalytics(req, res) {
-    //const { ativityID } = req.body;
+    const activityID = req.params.activityID;
 
     //Implementar busca na BD que devolve os parametros da atividade com id xxxx
 
     const sendTestAnalytics = [
       {
         analytic1: "Setting 1",
+        type:"text/plain"
       },
       {
         analytic2: "Setting 1",
+        type:"text/plain"
       },
     ];
 
     return res.status(200).json(sendTestAnalytics);
   }
+
+
+  //4. Analytics de atividade
+  //devolve analytics_url
+  async GetActivityAnalytics(req, res){
+    const { activityID } = req.body;
+    console.log(req)
+    const sendTestAnalytics = [
+      {
+        "activityID":activityID,
+        "inveniraStdID": 1001,
+      },
+      {
+        "activityID":activityID,
+        "inveniraStdID": 1002,
+      }
+    ];
+    console.log(sendTestAnalytics)
+    return res.status(200).json(sendTestAnalytics);
+  } 
+
+  
 }
 
 module.exports = new ActivityController();
--- src/routes.js
+++ src/routes.js
@@ -6,12 +6,24 @@ routes.get("/", (req, res) => {
   return res.status(200).json({ message: "Server is online" });
 });
 
-routes.get("/pagina-config", (req, res) => {
-  res.sendFile("views/config_page.html", { root: __dirname });
+//1. Página de configuração de uma atividade
+//config_url: URL da página do Activity Provider que permite configurar a atividade;
+routes.get("/pagina-config.html", (req, res) =>{
+  // Esta página não guarda nada no Activity Provider: é apenas usado pela Inven!RA, que o usa para apresentar a página de configuração ao utilizador.
+  res.sendFile("Views/config_page.html", { root: __dirname });
 });
-
 routes.get("/pagina-params", Controller.SendParams);
 
-routes.get("/activity-page", Controller.SendActivityPage);
+//3
+routes.post("/activity-page", Controller.SendActivityPageURL);
+routes.get("/activity-page", Controller.GetActivityPage);
+
+routes.post("/analytics", Controller.GetActivityAnalytics);
+
+routes.get("/analytics/:activityID", Controller.GetAnalytics);
+
+
+
+
 
 module.exports = routes;
