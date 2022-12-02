b358f534b2a7eeb5ec4a2035d8aea9fa49ce644d
Filipe Ferreira <filipeferreira96@hotmail.com>
2022-12-02 23:54:48 +0000

    console.log

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 const app = require("./app");
 const DB = require("./config/database");
-const Logger = require('./utils/logger');
+const Logger = require("./utils/logger");
 const port = process.env.PORT || 5000;
 
 //altera a configuração default do singleton Logger e este singlenton fica com esta nova configuração em todo lado!
 Logger.setConfig({
-    dateFormat: new Date().toLocaleString()
+  dateFormat: new Date().toLocaleString(),
 });
 
-DB.StartDB(); ///starts database
+DB.StartDB(); //liga a DB e utiliza singleton
 
 app.listen(port, () => Logger.info(`Server is running on Port ${port}`));
