a4f1afccad6c686123959f4649800c3eac414c04
Filipe Ferreira <filipeferreira96@hotmail.com>
2023-01-11 22:50:24 +0000

    added image field and new fe state

--- frontend/src/pages/Activity.js
+++ frontend/src/pages/Activity.js
@@ -1,20 +1,18 @@
-import { useEffect } from "react"
-import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 // nested routes
 
 export default function Activity() {
   let { activityID, inveniraStdID } = useParams();
-  console.log(activityID, inveniraStdID )
-  useEffect(() => {
-    
-  }, [])
+  const [params, setParams] = useState({});
+
+  console.log(activityID, inveniraStdID);
+  useEffect(() => {}, []);
 
   return (
     <div className="content">
-      <h2>Ativity { activityID }</h2>
-
-  
+      <h2>Ativity {activityID}</h2>
     </div>
-  )
+  );
 }
--- src/Controllers/Activity.js
+++ src/Controllers/Activity.js
@@ -3,16 +3,13 @@ const path = require("path");
 class ActivityController {
   //1.parâmetros respetivos
   async SendParams(req, res) {
-    //const { ativityID } = req.body;
-
-    //Implementar busca na BD que devolve os parametros da atividade com id xxxx
-
     const sendTestParams = [
       { name: "title", type: "text/plain" },
       { name: "summary", type: "text/plain" },
       { name: "documentation", type: "array" },
       { name: "correctAnswer", type: "array" },
       { name: "tipInfo", type: "text/plain" },
+      { name: "imgUrl", type: "text/plain" },
     ];
 
     return res.status(200).json(sendTestParams);
@@ -22,8 +19,6 @@ class ActivityController {
   // analytics_list_url: URL para um Web service que devolve a lista de analytics que o Activity Provider recolherá
   async GetAnalytics(req, res) {
     const activityID = req.params.activityID;
-
-    //Implementar busca na BD que devolve os parametros da atividade com id xxxx
 
     const sendTestAnalytics = [
       {
@@ -57,7 +52,9 @@ class ActivityController {
 
   async DeployActivityPost(req, res) {
     const { activityID, inveniraStdID, json_params } = req.body;
+    console.log(activityID, inveniraStdID, json_params);
 
+    //valida o conteudo do body
     if (!activityID) {
       return res.status(400).json({ message: "Invalid activity ID." });
     }
@@ -67,6 +64,8 @@ class ActivityController {
     if (!json_params) {
       return res.status(400).json({ message: "Invalid json_params." });
     }
+
+    //inserir dados do json params na bd para mais tarde o frontend utilizar estes dados
 
     res.status(200).json({ deployURL: `https://${req.headers.host}/activity/${activityID}/${inveniraStdID}` });
   }
